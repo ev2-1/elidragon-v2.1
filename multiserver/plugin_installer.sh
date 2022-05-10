a23de7766857310d9de35ff1744c72a97d075491
Edzell <derz@elidragon.com>
2022-05-10 22:06:09 +0200

    multiserver

---
+++ multiserver/plugin_installer.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+mkdir -p /go/proxy/cmd/mt-multiserver-proxy/plugins/
+
+for folder in $(ls /go/plugin_src/)
+do
+	echo building $folder
+	cd /go/plugin_src/$folder
+	go build -buildmode=plugin
+	cp *.so /go/proxy/cmd/mt-multiserver-proxy/plugins/
+done
+
+ls /go/proxy/cmd/mt-multiserver-proxy/plugins/
+
